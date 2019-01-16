1d768e9bd854774f9e11bbfb668d95efa5364a87
Jon Brohauge <jnz@topdanmark.dk>
2019-01-16 12:54:08 +0100

    import socket

--- dk/legevognen/pijenmon/consul_utilities.py
+++ dk/legevognen/pijenmon/consul_utilities.py
@@ -4,6 +4,8 @@ Class Consul Utilities
 Manage communication with consul
 
 """
+import socket
+
 try:
     from consul import Consul
 except ImportError:
@@ -12,5 +14,22 @@ except ImportError:
 
 class ConsulUtilities:
     """Class containing supporting methods"""
-    def __init__(self, consul=None):
+    def __init__(self, consul=None, hostname=None, jenkins=None):
         self._consul = consul or Consul()
+        if hostname is None:
+            self.set_hostname()
+        else:
+            self.hostname = hostname
+        if jenkins is None:
+            jenkins = 'localhost'
+        self.configuration = {'hostname': hostname, 'jenkins_url': jenkins}
+
+    def set_hostname(self, hostname=None):
+        """Set the hostname"""
+        if hostname is None:
+            hostname = socket.gethostname()  # pragma: no cover
+        self.hostname = hostname.lower()
+
+    def get_hostname(self):
+        """Get the hostname"""
+        return self.hostname
